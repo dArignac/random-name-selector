948629332480bb7e5df6376bd67bbec857423198
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-11-24 11:36:09 +0100

    added full publication

--- build/publish.sh
+++ build/publish.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-[ -d "./output" ] && rm ./output/*
-mkdir -p ./output
+[ -d "./output" ] && rm -r ./output/*
+mkdir -p ./output/src
+cp -r ./font ./output/
+cp -r ./assets ./output/
 build/minify -o ./output/index.html index.html
+build/minify -o ./output/src/script.js src/script.js
+build/minify -o ./output/src/style.css src/style.css
