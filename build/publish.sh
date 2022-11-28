a4e2d9a40bac26ae87599b65a1faf04839a389cc
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-11-28 11:17:55 +0100

    Font handling improvements (#5)
    * removed web font #4
    
    * changed removal icon #4
    
    * changed image to inline base64 #4

--- build/publish.sh
+++ build/publish.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 [ -d "./output" ] && rm -r ./output/*
 mkdir -p ./output/src
-cp -r ./font ./output/
 cp -r ./assets ./output/
 build/minify -o ./output/index.html index.html
 build/minify -o ./output/src/script.js src/script.js
