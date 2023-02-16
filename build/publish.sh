d2a0fa269cc6dfef79c5d404ac0d5d97a449b797
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2023-02-16 11:46:29 +0100

    avoid cashing of styles and scripts

--- build/publish.sh
+++ build/publish.sh
@@ -2,6 +2,10 @@
 [ -d "./output" ] && rm -r ./output/*
 mkdir -p ./output/src
 cp -r ./assets ./output/
+md5_script=$(md5sum src/script.js | awk '{print $1}' | cut -c 1-8)
+md5_style=$(md5sum src/style.css | awk '{print $1}' | cut -c 1-8)
 build/minify -o ./output/index.html index.html
-build/minify -o ./output/src/script.js src/script.js
-build/minify -o ./output/src/style.css src/style.css
+sed -i "s/script.js/script.${md5_script}.js/" ./output/index.html
+sed -i "s/style.css/style.${md5_style}.css/" ./output/index.html
+build/minify -o ./output/src/script.${md5_script}.js src/script.js
+build/minify -o ./output/src/style.${md5_style}.css src/style.css
